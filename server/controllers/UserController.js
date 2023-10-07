4ab6e5552a3f08a3b9fc3e920b46f247224406e1
Luu Vu <vu7178@gmail.com>
2023-10-07 21:31:00 +0700

    firstCommit

---
+++ server/controllers/UserController.js
@@ -0,0 +1,89 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Register
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if both username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    }
+
+    // Check username length
+    if (username.trim().length < 4 || username.trim().length > 20) {
+      return res.status(400).json({
+        msg: "Username must be between 4 and 20 characters long",
+      });
+    }
+
+    // Check password length
+    if (password.trim().length < 4) {
+      return res.status(400).json({
+        msg: "Password must be at least 4 characters long",
+      });
+    }
+    const checkUserNames = username.trim().toLowerCase();
+    // Check if the username already exists
+    const existUser = await User.findOne({ username: checkUserNames });
+    if (existUser) {
+      return res.status(400).json({ msg: "Username already exists" });
+    }
+
+    // Hash the password
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    // Create a new User instance
+    const newUser = new User({
+      username: username.trim().toLowerCase(),
+      password: hashedPass,
+    });
+
+    // Save the user to the database
+    const savedUser = await newUser.save();
+
+    // Create a JWT token for the user
+    const token = jwt.sign({ user: savedUser }, process.env.JWT_SECRET);
+
+    // Respond with success message, user data, and token
+    res.json({ msg: "User created", user: savedUser, token });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if both username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    }
+
+    // Check if the username exists
+    const user = await User.findOne({
+      username: username.trim().toLowerCase(),
+    });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    // Check if the password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    // Create a JWT token for the user
+    const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+    // Respond with success message, user data, and token
+    res.json({ msg: "User logged in", user, token });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+module.exports = { register, login };
---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+const UserRouter = require("./routes/auth.js");
+// Create a new Express app
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config(path.join(__dirname, ".env"));
+const URI = process.env.MONGODB_URI;
+// Connect to MongoDB
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+app.use("/", UserRouter);
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
+
+// Define routes
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
